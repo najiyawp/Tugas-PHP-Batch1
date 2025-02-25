ffc0aedacd9ba6783320935b0127fd22d3e25676
najiyawp <najiyarizqi70@gmail.com>
2025-02-25 22:00:05 +0700

    second commit

--- skenario2.php
+++ skenario2.php
@@ -21,15 +21,15 @@
         if ($jam >= "00:00" && $jam < "04:00") {
             echo "<p>$jam = Dini hari</p>";
         } elseif ($jam >= "04:00" && $jam < "10:00") {
-            echo "<p>$jam = Pagi</p>";
+            echo "<p>$jam = Pagi hari</p>";
         } elseif ($jam >= "10:00" && $jam < "15:00") {
-            echo "<p>$jam = Siang</p>";
+            echo "<p>$jam = Siang hari</p>";
         } elseif ($jam >= "15:00" && $jam < "17:30") {
-            echo "<p>$jam = Sore</p>";
+            echo "<p>$jam = Sore hari</p>";
         } elseif ($jam >= "17:30" && $jam < "18:30") {
             echo "<p>$jam = Petang</p>";
         } elseif ($jam >= "18:30" && $jam < "24:00") {
-            echo "<p>$jam = Malam</p>";
+            echo "<p>$jam = Malam hari</p>";
         } else {
             echo "<p>Dunia lain</p>";
         }
